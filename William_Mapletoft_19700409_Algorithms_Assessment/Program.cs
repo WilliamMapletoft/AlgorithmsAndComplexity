1a14fe00e15b671c038f12418743d1415dafa3ad
Will <willmapletof@gmail.com>
2020-03-07 21:57:49 +0000

    Code should be finished.

--- William_Mapletoft_19700409_Algorithms_Assessment/Program.cs
+++ William_Mapletoft_19700409_Algorithms_Assessment/Program.cs
@@ -12,12 +12,15 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
     {
         public static string SelectedNet = "Net_1_256.txt";
         public static string[] arr = File.ReadAllLines(SelectedNet);
+        public static string[] arr2 = File.ReadAllLines(SelectedNet);
         public static int[] DownloadedArray = arr.Select(int.Parse).ToArray();
+        public static int[] DownloadedArray2 = arr.Select(int.Parse).ToArray();
+        public static bool Debug = false;
         static void Main(string[] args)
         {
             var TempArray = new int[DownloadedArray.Length];
             DownloadedArray.CopyTo(TempArray, 0);
-            int[] SortedArray = SortMethods.BubbleSort(TempArray);
+            int[] SortedArray = SortMethods.HeapSort(TempArray, Debug);
             Console.WriteLine($"Selected net: {SelectedNet}");
             switch(Menu())
             {
@@ -25,13 +28,13 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                 //case 2: SortedArray = SortMethods.BubbleSort(DownloadedArray); EveryTenth(SortedArray); break;
                 case 2:
                     DownloadedArray.CopyTo(TempArray, 0);
-                    SortedArray = SortMethods.InsertionSort(TempArray);
+                    SortedArray = SortMethods.InsertionSort(TempArray, Debug);
                     DownloadedArray.CopyTo(TempArray, 0);
-                    SortedArray = SortMethods.BubbleSort(TempArray);
+                    SortedArray = SortMethods.BubbleSort(TempArray, Debug);
                     DownloadedArray.CopyTo(TempArray, 0);
-                    SortedArray = SortMethods.MergeSort(TempArray);
+                    SortedArray = SortMethods.MergeSort(TempArray, Debug);
                     DownloadedArray.CopyTo(TempArray, 0);
-                    SortedArray = SortMethods.HeapSort(TempArray);
+                    SortedArray = SortMethods.HeapSort(TempArray, Debug);
                     if (Convert.ToInt32(new string(SelectedNet[7],1)) == 5)
                     {
                         EveryTenth(SortedArray);
@@ -42,6 +45,22 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                     }
                     break;
                 case 3:
+                    DownloadedArray.CopyTo(TempArray, 0);
+                    SortedArray = SortMethods.BubbleSortDown(TempArray, Debug);
+                    DownloadedArray.CopyTo(TempArray, 0);
+                    SortedArray = SortMethods.InsertionSortDown(TempArray, Debug);
+                    DownloadedArray.CopyTo(TempArray, 0);
+                    SortedArray = SortMethods.MergeSortDown(TempArray, Debug);
+                    DownloadedArray.CopyTo(TempArray, 0);
+                    SortedArray = SortMethods.HeapSortDown(TempArray, Debug);
+                    if (Convert.ToInt32(new string(SelectedNet[7], 1)) == 5)
+                    {
+                        EveryTenth(SortedArray);
+                    }
+                    else
+                    {
+                        EveryFifty(SortedArray);
+                    }
                     break;
                 case 4:
                     Console.WriteLine("Please enter a value to search for.");
@@ -65,6 +84,13 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                         Console.WriteLine($"InterpolationSearch = Item found in position {position2}");
                     }
                     break;
+                case 5:
+                    SelectNets();
+                    break;
+                case 6:
+                    if (Debug == false) { Debug = true; }
+                    else  { Debug = false; }
+                    break;
             }
             //int[] SortedArray = SortMethods.BubbleSort(Net1_256);
             //EveryTenth(SortedArray);
@@ -81,6 +107,8 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                 Console.WriteLine("2: Sort in ascending order");
                 Console.WriteLine("3: Sort in descrending order");
                 Console.WriteLine("4: Search the array for a user defined value");
+                Console.WriteLine("5: Merge two nets and perform options 2-4");
+                Console.WriteLine("6: Toggle Debug");
                 try
                 {
                     choice = int.Parse(Console.ReadLine());
@@ -91,7 +119,7 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                     Menu();
                 }
             }
-            while (choice < 1 || choice > 4);
+            while (choice < 1 || choice > 6);
             return choice;
         }
 
@@ -153,6 +181,129 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
             }
             while (choice < 1 || choice > 6);
             return net;
+        }
+
+        static void SelectNets()
+        {
+            string net = "";
+            string net2 = "";
+            int choice = 1;
+            do
+            {
+                Console.WriteLine("Choose from the 6 following options:");
+                Console.WriteLine("1: Net_1_256");
+                Console.WriteLine("2: Net_1_2048");
+                Console.WriteLine("3: Net_2_256");
+                Console.WriteLine("4: Net_2_2048");
+                Console.WriteLine("5: Net_3_256");
+                Console.WriteLine("6: Net_3_2048");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input.");
+                    SelectNet();
+                }
+                switch (choice)
+                {
+                    case 1: net = "Net_1_256.txt"; break;
+                    case 2: net = "Net_1_2048.txt"; break;
+                    case 3: net = "Net_2_256.txt"; break;
+                    case 4: net = "Net_2_2048.txt"; break;
+                    case 5: net = "Net_3_256.txt"; break;
+                    case 6: net = "Net_3_2048.txt"; break;
+                }
+                arr = File.ReadAllLines(net);
+                DownloadedArray = arr.Select(int.Parse).ToArray();
+            }
+            while (choice < 1 || choice > 6);
+
+            do
+            {
+                Console.WriteLine("Choose from the 6 following options:");
+                Console.WriteLine("1: Net_1_256");
+                Console.WriteLine("2: Net_1_2048");
+                Console.WriteLine("3: Net_2_256");
+                Console.WriteLine("4: Net_2_2048");
+                Console.WriteLine("5: Net_3_256");
+                Console.WriteLine("6: Net_3_2048");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input.");
+                    SelectNet();
+                }
+                switch (choice)
+                {
+                    case 1: net2 = "Net_1_256.txt"; break;
+                    case 2: net2 = "Net_1_2048.txt"; break;
+                    case 3: net2 = "Net_2_256.txt"; break;
+                    case 4: net2 = "Net_2_2048.txt"; break;
+                    case 5: net2 = "Net_3_256.txt"; break;
+                    case 6: net2 = "Net_3_2048.txt"; break;
+                }
+                arr2 = File.ReadAllLines(net2);
+                DownloadedArray2 = arr2.Select(int.Parse).ToArray();
+            }
+            while (choice < 1 || choice > 6);
+            int[] MergedArray = new int[DownloadedArray.Length + DownloadedArray2.Length];
+            Array.Copy(DownloadedArray, MergedArray, DownloadedArray.Length);
+            Array.Copy(DownloadedArray2, 0, MergedArray, DownloadedArray.Length, DownloadedArray2.Length);
+            if (net == net2)
+            {
+                Console.WriteLine("Please select two different nets.");
+                SelectNets();
+            }
+
+            var TempArray = new int[MergedArray.Length];
+            int[] SortedArray = SortMethods.HeapSort(TempArray, Debug);
+            Console.WriteLine($"Selected nets: {net} & {net2}");
+            Console.WriteLine("Descending sort:");
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.BubbleSortDown(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.InsertionSortDown(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.MergeSortDown(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.HeapSortDown(TempArray, Debug);
+            EveryTenth(SortedArray);
+            Console.WriteLine("Ascending sort:");
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.InsertionSort(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.BubbleSort(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.MergeSort(TempArray, Debug);
+            MergedArray.CopyTo(TempArray, 0);
+            SortedArray = SortMethods.HeapSort(TempArray, Debug);
+            EveryTenth(SortedArray);
+
+            Console.WriteLine("Please enter a value to search for.");
+            int value = int.Parse(Console.ReadLine());
+            int position = SearchMethods.BinarySearch(SortedArray, value);
+            if (position == -1)
+            {
+                Console.WriteLine("BinarySearch = Searched item not found.");
+            }
+            else
+            {
+                Console.WriteLine($"BinarySearch = Item found in position {position}");
+            }
+            int position2 = SearchMethods.InterPolationSearch(SortedArray, value);
+            if (position2 == -1)
+            {
+                Console.WriteLine("InterpolationSearch = Searched item not found.");
+            }
+            else
+            {
+                Console.WriteLine($"InterpolationSearch = Item found in position {position2}");
+            }
         }
     }
 }
--- William_Mapletoft_19700409_Algorithms_Assessment/SortMethods.cs
+++ William_Mapletoft_19700409_Algorithms_Assessment/SortMethods.cs
@@ -11,7 +11,7 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
 
         public static int MergeCounter = 0;
         public static int HeapCounter = 0;
-        public static int[] BubbleSort(int[] Input)
+        public static int[] BubbleSort(int[] Input, bool debug)
         {
             int counter = 0;
             int temp = 0;
@@ -23,6 +23,7 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                 {
                     if (Input[i + 1] < Input[i])
                     {
+                        if (debug == true) { Console.WriteLine($"Swapping items {Input[i]} and {Input[i + 1]}"); }
                         temp = Input[i + 1];
                         Input[i + 1] = Input[i];
                         Input[i] = temp;
@@ -33,13 +34,34 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
             }
             
             Console.WriteLine($"Bubble sort operations: {counter}");
-            foreach (int each in Input)
-            {
-                Console.WriteLine(each);
-            }
             return Input;
         }
-        public static int[] InsertionSort(int[] Input)
+        public static int[] BubbleSortDown(int[] Input, bool debug)
+        {
+            int counter = 0;
+            int temp = 0;
+            bool sorted = false;
+            while (sorted == false)
+            {
+                sorted = true;
+                for (int i = 0; i < Input.Length - 1; i++)
+                {
+                    if (Input[i + 1] > Input[i])
+                    {
+                        if (debug == true) { Console.WriteLine($"Swapping items {Input[i]} and {Input[i + 1]}"); }
+                        temp = Input[i + 1];
+                        Input[i + 1] = Input[i];
+                        Input[i] = temp;
+                        sorted = false;
+                    }
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Bubble sort operations: {counter}");
+            return Input;
+        }
+        public static int[] InsertionSort(int[] Input, bool debug)
         {
             int counter = 0;
             int numSorted = 1;
@@ -52,6 +74,34 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                     counter++;
                     if (temp < Input[i - 1])
                     {
+                        if (debug == true) { Console.WriteLine($"Swapping items {Input[i]} and {Input[i - 1]}"); }
+                        Input[i] = Input[i - 1];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Input[i] = temp;
+                numSorted++;
+            }
+            Console.WriteLine($"Insertion sort operations : {counter}");
+            return Input;
+        }
+        public static int[] InsertionSortDown(int[] Input, bool debug)
+        {
+            int counter = 0;
+            int numSorted = 1;
+            int i;
+            while (numSorted < Input.Length)
+            {
+                int temp = Input[numSorted];
+                for (i = numSorted; i > 0; i--)
+                {
+                    counter++;
+                    if (temp > Input[i - 1])
+                    {
+                        if (debug == true) { Console.WriteLine($"Swapping items {Input[i]} and {Input[i - 1]}"); }
                         Input[i] = Input[i - 1];
                     }
                     else
@@ -66,7 +116,7 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
             return Input;
         }
 
-        private static void Merge(int[] Input, int[] temp, int low, int middle, int high)
+        private static void Merge(int[] Input, int[] temp, int low, int middle, int high, bool debug)
         {
             int ResultIndex = low;
             int TempIndex = low;
@@ -76,6 +126,30 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                 MergeCounter++;
                 if (Input[DestinationIndex] < temp[TempIndex])
                 {
+                    if (debug == true) { Console.WriteLine($"Swapping items {Input[ResultIndex + 1]} and {Input[DestinationIndex + 1]}"); }
+                    Input[ResultIndex++] = Input[DestinationIndex++];
+                }
+                else
+                {
+                    Input[ResultIndex++] = temp[TempIndex++];
+                }
+            }
+            while (TempIndex < middle)
+            {
+                Input[ResultIndex++] = temp[TempIndex++];
+            }
+        }
+        private static void MergeDown(int[] Input, int[] temp, int low, int middle, int high, bool debug)
+        {
+            int ResultIndex = low;
+            int TempIndex = low;
+            int DestinationIndex = middle;
+            while (TempIndex < middle && DestinationIndex <= high)
+            {
+                MergeCounter++;
+                if (Input[DestinationIndex] > temp[TempIndex])
+                {
+                    if (debug == true) { Console.WriteLine($"Swapping items {Input[ResultIndex + 1]} and {Input[DestinationIndex + 1]}"); }
                     Input[ResultIndex++] = Input[DestinationIndex++];
                 }
                 else
@@ -89,7 +163,7 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
             }
         }
 
-        private static void MergeSortRecursive(int[] Input, int[] temp, int low, int high)
+        private static void MergeSortRecursive(int[] Input, int[] temp, int low, int high, bool debug)
         {
             MergeCounter++;
             int n = high - low + 1;
@@ -101,27 +175,50 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
             {
                 temp[i] = Input[i];
             }
-            MergeSortRecursive(temp, Input, low, middle - 1);
-            MergeSortRecursive(Input, temp, middle, high);
-            Merge(Input, temp, low, middle, high);
+            MergeSortRecursive(temp, Input, low, middle - 1, debug);
+            MergeSortRecursive(Input, temp, middle, high, debug);
+            Merge(Input, temp, low, middle, high, debug);
         }
+        private static void MergeSortRecursiveDown(int[] Input, int[] temp, int low, int high, bool debug)
+        {
+            MergeCounter++;
+            int n = high - low + 1;
+            int middle = low + n / 2;
+            int i;
 
-        public static int[] MergeSort(int[] Input)
+            if (n < 2) return;
+            for (i = low; i < middle; i++)
+            {
+                temp[i] = Input[i];
+            }
+            MergeSortRecursiveDown(temp, Input, low, middle - 1, debug);
+            MergeSortRecursiveDown(Input, temp, middle, high, debug);
+            MergeDown(Input, temp, low, middle, high, debug);
+        }
+        public static int[] MergeSort(int[] Input, bool debug)
         {
             int[] temp = new int[Input.Length];
             MergeCounter = 0;
-            MergeSortRecursive(Input, temp, 0, Input.Length - 1);
+            MergeSortRecursive(Input, temp, 0, Input.Length - 1, debug);
+            Console.WriteLine($"Merge sort operations : {MergeCounter}");
+            return Input;
+        }
+        public static int[] MergeSortDown(int[] Input, bool debug)
+        {
+            int[] temp = new int[Input.Length];
+            MergeCounter = 0;
+            MergeSortRecursiveDown(Input, temp, 0, Input.Length - 1, debug);
             Console.WriteLine($"Merge sort operations : {MergeCounter}");
             return Input;
         }
 
-        public static int[] HeapSort(int[] Input)
+        public static int[] HeapSort(int[] Input, bool debug)
         {
             int n = Input.Length;
 
             for (int i = n / 2 - 1; i >= 0; i--)
             {
-                Heapify(Input, n, i);
+                Heapify(Input, n, i, debug);
             }
 
             for(int i = n-1; i>=0;i--)
@@ -130,13 +227,33 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
                 Input[0] = Input[i];
                 Input[i] = temp;
 
-                Heapify(Input, i, 0);
+                Heapify(Input, i, 0, debug);
+            }
+            Console.WriteLine($"Heap sort operations : {HeapCounter}");
+            return Input;
+        }
+        public static int[] HeapSortDown(int[] Input, bool debug)
+        {
+            int n = Input.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                HeapifyDown(Input, n, i, debug);
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = Input[0];
+                Input[0] = Input[i];
+                Input[i] = temp;
+
+                HeapifyDown(Input, i, 0, debug);
             }
             Console.WriteLine($"Heap sort operations : {HeapCounter}");
             return Input;
         }
 
-        private static void Heapify(int[] Input, int n, int i)
+        private static void Heapify(int[] Input, int n, int i, bool debug)
         {
             HeapCounter++;
             int Largest = i;
@@ -155,11 +272,39 @@ namespace William_Mapletoft_19700409_Algorithms_Assessment
 
             if(Largest != i)
             {
+                if (debug == true) {  Console.WriteLine($"Swapping items {Input[i]} and {Input[Largest]}"); }
                 int swap = Input[i];
                 Input[i] = Input[Largest];
                 Input[Largest] = swap;
 
-                Heapify(Input, n, Largest);
+                Heapify(Input, n, Largest, debug);
+            }
+        }
+        private static void HeapifyDown(int[] Input, int n, int i, bool debug)
+        {
+            HeapCounter++;
+            int Largest = i;
+            int Left = 2 * i + 1;
+            int Right = 2 * i + 2;
+
+            if (Left < n && Input[Left] < Input[Largest])
+            {
+                Largest = Left;
+            }
+
+            if (Right < n && Input[Right] < Input[Largest])
+            {
+                Largest = Right;
+            }
+
+            if (Largest != i)
+            {
+                if (debug == true){ Console.WriteLine($"Swapping items {Input[i]} and {Input[Largest]}"); }
+                int swap = Input[i];
+                Input[i] = Input[Largest];
+                Input[Largest] = swap;
+
+                HeapifyDown(Input, n, Largest, debug);
             }
         }
     }
